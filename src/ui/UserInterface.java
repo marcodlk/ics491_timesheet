1a9151bc0ba3dcff99a665f592b5540970a31a49
Marco de Lannoy Kobayashi <markobrazil93@hawaii.edu>
2017-08-08 22:19:37 -1000

    Checkstyle-mindful fixes

--- src/ui/UserInterface.java
+++ src/ui/UserInterface.java
@@ -2,9 +2,7 @@ package ui;
 
 //java
 import java.lang.System;
-import java.io.*;
 import java.util.NoSuchElementException;
-import java.util.*;
 import java.util.Scanner;
 //local
 import security.LoginManager;
@@ -43,13 +41,13 @@ public class UserInterface implements Runnable {
     System.out.println("");
   }
 
-	private int punchClock(int mode) {
-		Scanner reader = new Scanner(System.in);
-		String opt;
-		boolean goBack = false;
-		do {
-			displayPunchClockMenu(mode);
-			System.out.print("> ");
+  private int punchClock(int mode) {
+    Scanner reader = new Scanner(System.in);
+    String opt;
+    boolean goBack = false;
+    do {
+      displayPunchClockMenu(mode);
+      System.out.print("> ");
       try {
         opt = reader.nextLine();
       } catch (NoSuchElementException e) {
@@ -60,65 +58,61 @@ public class UserInterface implements Runnable {
       
       switch (opt) { 
         case "1":
-				  if (mode == 0) {
-						//clocking in, begin recording session, send clock-in to timesheet
-						String timeIn = 
-							new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new java.util.Date());
-						System.out.println("+----------------------------------------------------------");
-						System.out.println("| Session started at: " + timeIn);
-						System.out.println("+----------------------------------------------------------");
-						//display pseudocode for future development
-						//System.out.println(". . .");
-						//System.out.println("  PreparedStatement ps = con.prepareStatement(");
-						//System.out.println("  INSERT INTO timesheet (user_ID, time_in)  VALUES  (?,?)\n");
-						//System.out.println("  ps.setString(1,token.id); ps.setString(2,timeIn);"); 
-						//System.out.println("");
-						//System.out.println(". . .");
-					} else { //mode should be 1 here
-						//clocking out, record session terminated, send clock-out to timesheet
-						String timeOut = 
-							new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new java.util.Date());
-						System.out.println("+----------------------------------------------------------");
-						System.out.println("| Session ended at: " + timeOut);
-						System.out.println("+----------------------------------------------------------");
-						//display pseudocode for future development
-						//System.out.println(". . .");
-						//System.out.println("  PreparedStatement ps = con.prepareStatement(");
-						//System.out.println("  INSERT INTO timesheet (user_ID, time_out)  VALUES  ( ?,? )\n");
-						//System.out.println("  ps.setString(1,token.id); ps.setString(2,timeOut);"); 
-						//System.out.println("");
-						//System.out.println(". . .");
-					}
+          if (mode == 0) {
+            //clocking in, begin recording session, send clock-in to timesheet
+            String timeIn = 
+                new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new java.util.Date());
+            System.out.println("+----------------------------------------------------------");
+            System.out.println("| Session started at: " + timeIn);
+            System.out.println("+----------------------------------------------------------");
+            //display pseudocode for future development
+            //System.out.println(". . .");
+            //System.out.println("  PreparedStatement ps = con.prepareStatement(");
+            //System.out.println("  INSERT INTO timesheet (user_ID, time_in)  VALUES  (?,?)\n");
+            //System.out.println("  ps.setString(1,token.id); ps.setString(2,timeIn);"); 
+            //System.out.println("");
+            //System.out.println(". . .");
+          } else { //mode should be 1 here
+            //clocking out, record session terminated, send clock-out to timesheet
+            String timeOut = 
+                new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new java.util.Date());
+            System.out.println("+----------------------------------------------------------");
+            System.out.println("| Session ended at: " + timeOut);
+            System.out.println("+----------------------------------------------------------");
+            //display pseudocode for future development
+            //System.out.println(". . .");
+            //System.out.println("  PreparedStatement ps = con.prepareStatement(");
+            //System.out.println("  INSERT INTO timesheet (user_ID, time_out)  VALUES  ( ?,? )\n");
+            //System.out.println("  ps.setString(1,token.id); ps.setString(2,timeOut);"); 
+            //System.out.println("");
+            //System.out.println(". . .");
+          }
           mode = (mode + 1) % 2;
-					assert (mode == 0) || (mode == 1);
+          assert (mode == 0) || (mode == 1);
           break;
         case "0": 
-				  goBack = true;
+          goBack = true;
           break;
         default:
           System.out.println("Unrecognized command");
-			}
-		} while (!goBack);
+      }
+    } while (!goBack);
 
-		return mode;
-	}  
-
-  private Token getToken() {
-	return this.token;
-  }
+    return mode;
+  }  
 
   private void displayPunchClockMenu(int mode) {
-		assert (mode == 0) || (mode == 1);
+    assert (mode == 0) || (mode == 1);
     System.out.println("+---+------------------------------------------------------");
     System.out.println("|     PUNCH CLOCK                                          ");
     System.out.println("+---+------------------------------------------------------");
     System.out.println("|     Select one of the following options:                 ");
     System.out.println("+---+------------------------------------------------------");
-		if (mode == 0) { //not clocked in
-			System.out.println("| 1 | Clock-in ");
-		} else if (mode == 1) { //already clocked in
-			System.out.println("| 1 | Clock-out ");
-		}
+    if (mode == 0) { //not clocked in
+      System.out.println("| 1 | Clock-in ");
+    } else if (mode == 1) { //already clocked in
+      System.out.println("| 1 | Clock-out ");
+    }
     System.out.println("+---+------------------------------------------------------");
     System.out.println("| 0 | Return to Main Menu                                  ");
     System.out.println("+---+------------------------------------------------------");
@@ -155,15 +149,24 @@ public class UserInterface implements Runnable {
   private void displayInfoMenu(int mode) {
     System.out.print("Here you can request timesheet info");
     if (mode == 0) {
-      System.out.print("... but ONLY about yourself. \n\n For example if you wanted to check for your current time clocks: \n\n SELECT user_ID, date, time_in, time_out FROM timesheet\n\t WHERE user_ID =  this.getUserID() \n");
+      System.out.print("... but ONLY about yourself. "
+          + "\n\n For example if you wanted to check for your current time clocks: "
+          + "\n\n SELECT user_ID, date, time_in, time_out FROM timesheet\n\t "
+          + "WHERE user_ID = token.id \n");
     } else if (mode == 1) {
-      System.out.print("... about EVERYONE! \n\n For example if you wanted to search for an employee: \n\n SELECT * FROM timesheet\n\t WHERE user_ID =  this.getUserID()\n");
+      System.out.print("... about EVERYONE! "
+          + "\n\n For example if you wanted to search for an employee: "
+          + "\n\n SELECT * FROM timesheet\n\t "
+          + "WHERE user_ID = token.id \n");
     }
     //System.out.println(" !");
   }
 
   private void displayRecordsMenu() {
-    System.out.println("Here you can manage records! \n\nFor example including a new user: INSERT INTO users (username,password,privilege_level)  VALUES  ( codosa, champ123!, 1 )\n");
+    System.out.println("Here you can manage records! "
+        + "\n\nFor example including a new user: "
+        + "INSERT INTO users (username,password,privilege_level) "
+        + "VALUES ( admin, champ123!, 1 )\n");
   }
 
   public void runAsAdmin() {
@@ -210,7 +213,7 @@ public class UserInterface implements Runnable {
     Scanner reader = new Scanner(System.in);
     String buffer;
     boolean running = true;
-		int sessionStatus = 0;
+    int sessionStatus = 0;
 
     //run
     while (running) {
@@ -239,40 +242,40 @@ public class UserInterface implements Runnable {
     }
   }
 
-	private void loginScreen() {
+  private void loginScreen() {
     Scanner reader = new Scanner(System.in);
     System.out.println("+----------------------------------------------------------");
     System.out.println("| Please login:                                            ");
     System.out.println("+----------------------------------------------------------");
     System.out.print("| Username > ");
-		String username = reader.nextLine();
+    String username = reader.nextLine();
 
     System.out.println("+----------------------------------------------------------");
     System.out.print("| Password > ");
-		String password = reader.nextLine();
+    String password = reader.nextLine();
     System.out.println("+----------------------------------------------------------");
     System.out.println("");
-		this.token = LoginManager.authenticate(username,password);
-		while (this.token.getId() == -1) {
-			System.out.println("+----------------------------------------------------------");
-			System.out.println("| Invalid credentials... please try again:                 ");
-			System.out.println("+----------------------------------------------------------");
-			System.out.print("| Username > ");
-			username = reader.nextLine();
+    this.token = LoginManager.authenticate(username,password);
+    while (this.token.getId() == -1) {
+      System.out.println("+----------------------------------------------------------");
+      System.out.println("| Invalid credentials... please try again:                 ");
+      System.out.println("+----------------------------------------------------------");
+      System.out.print("| Username > ");
+      username = reader.nextLine();
 
-			System.out.println("+----------------------------------------------------------");
-			System.out.print("| Password > ");
-			password = reader.nextLine();
-			System.out.println("+----------------------------------------------------------");
-			System.out.println("");
-			this.token = LoginManager.authenticate(username,password);
-		}	
-	}
+      System.out.println("+----------------------------------------------------------");
+      System.out.print("| Password > ");
+      password = reader.nextLine();
+      System.out.println("+----------------------------------------------------------");
+      System.out.println("");
+      this.token = LoginManager.authenticate(username,password);
+    } 
+  }
 
   public void run() {
-		System.out.print("\033[2J\033[;H");
+    System.out.print("\033[2J\033[;H");
     displayGreetingSign();
-		loginScreen();
+    loginScreen();
     displayGreetingMessage();
     //run in mode respective to privilege level
     if (token.getPrivilegeLevel() == 1) {
